f64cda19e6fcd14f78cc61072a2b575f1ad90189
Cassinni4 <cassinni4@gmail.com>
2024-10-20 20:40:17 -0400

    Revert "don't make me call a mod!"
    This reverts commit d4aeb8002d47d02894a8fbc973b1158de04ab34f.

--- tools/unluac/unluac.cmd
+++
@@ -1,7 +0,0 @@
-set X=%1
-set X=%X:.lua=_dec.lua%
-set X=%X:.dnax=_dec.dnax%
-set X=%X:.sx=_dec.sx%
-set X=%X:.bmx=_dec.bmx%
-set X=%X:.btx=_dec.btx%
-java -jar "D:\brz\Breeze\tools\unluac\unluac.jar" %1 > %X%
