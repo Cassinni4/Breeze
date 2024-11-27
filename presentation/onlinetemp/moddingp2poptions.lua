3ba63f51c5479ad73fad7e133b59e028c54b67d5
Cassinni4 <cassinni4@gmail.com>
2024-11-26 19:17:40 -0500

    complete rewrite. NEVER REVERT!

--- presentation/onlinetemp/moddingp2poptions.lua
+++
@@ -1,8 +0,0 @@
--- moddingp2poptions.lua
-local ModdingP2POptions = {}
-
-function ModdingP2POptions:ShowWarning()
-    print("Warning: Modding options for P2P are currently unstable.")
-end
-
-return ModdingP2POptions
